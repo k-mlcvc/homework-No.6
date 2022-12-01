f3f0b05ddd174808f51ee025936082a1a12458c3
k-mlcvc <kristinamilicevic398@gmail.com>
2022-12-01 17:05:15 +0100

    Zadaća 6

---
+++ index.js
@@ -0,0 +1,25 @@
+// Deklariraj varijablu myString i spremi u nju neki string.
+// Uzmi sve znakove od 5. znaka pa do 10. znaka ne ukljucujuci 10. znak.
+// Ako je duljina stringa manja od 5. ispisi: Krace od 5!
+// Ako je string prazan ispisi: Prazan string!
+// Ako je duljina stringa 5 ispisi te znakove
+
+var myString = "Neki string";
+var subStr = (myString.substring(5,10));
+
+if (subStr.length < 5 && subStr.length != 0) {
+    console.log ("Kraće od 5!");
+} else if (subStr.length === 0) {
+    console.log("Prazan string!");
+} else if (subStr.length === 5) {
+    console.log(subStr);
+}
+
+// ILI
+if (subStr.length < 5 && subStr.length != 0) {
+    console.log ("Kraće od 5!");
+} else if (subStr === "") {
+    console.log("Prazan string!");
+} else if (subStr.length === 5) {
+    console.log(subStr);
+}
